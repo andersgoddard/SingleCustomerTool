344a6ec44debb4e5d87d26025c4c4205b2eea5f1
Andrew Goddard <andersgoddard@gmail.com>
2023-01-20 16:02:20 +0000

    Updated ContactMergerTest to test for merging more than one Contact

--- src/main/java/Contact/ContactMergerTest.java
+++ src/main/java/Contact/ContactMergerTest.java
@@ -8,16 +8,24 @@ import DatabaseFields.SimpleDatabaseFields;
 import Utilities.ContactFactory;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ContactMergerTest {
-    @Test
-    public void testMergeContacts(){
+    ContactList contacts;
+    ContactFactory factory;
+    @BeforeEach
+    public void setUp(){
+        contacts = ContactList.getInstance();
         Injector injector = Guice.createInjector(new ContactFactoryModule());
-        ContactFactory factory = injector.getInstance(ContactFactory.class);
+        this.factory = injector.getInstance(ContactFactory.class);
+    }
 
+    @Test
+    public void testMergeTwoContacts(){
         ContactInfo info = new ContactInfo();
         info.add(PhoneNumber.create("07881266969"));
         DatabaseFields fields1 = new SimpleDatabaseFields("Mr Andrew Goddard", info, null);
@@ -33,5 +41,41 @@ public class ContactMergerTest {
         assertEquals(andrew1.getUniqueIdentifier(), andrew2.getUniqueIdentifier());
         assertEquals(1, andrew1.getChildContacts().size());
         assertTrue(andrew1.getChildContacts().contains(andrew2));
+    }
+
+    @Test
+    public void testMergeThreeContacts(){
+        ContactInfo info = new ContactInfo();
+        info.add(PhoneNumber.create("07881266969"));
+        DatabaseFields fields1 = new SimpleDatabaseFields("Mr Andrew Goddard", info, null);
+        Contact andrew1 = factory.create(fields1);
+
+        ContactInfo info2 = new ContactInfo();
+        info2.add(EmailAddress.create("andersgoddard@gmail.com"));
+        DatabaseFields fields2 = new SimpleDatabaseFields("Mr A Goddard", info2, null);
+        Contact andrew2 = factory.create(fields2);
+
+        ContactInfo info3 = new ContactInfo();
+        info3.add(EmailAddress.create("aiandbgoddard@gmail.com"));
+        DatabaseFields fields3 = new SimpleDatabaseFields("Mr A Goddard", info3, null);
+        Contact andrew3 = factory.create(fields3);
+
+        assertNotEquals(andrew1.getUniqueIdentifier(), andrew2.getUniqueIdentifier());
+        assertNotEquals(andrew1.getUniqueIdentifier(), andrew3.getUniqueIdentifier());
+        assertNotEquals(andrew2.getUniqueIdentifier(), andrew3.getUniqueIdentifier());
+
+        ContactMerger.merge(andrew1, andrew2);
+        ContactMerger.merge(andrew1, andrew3);
+        assertEquals(andrew1.getUniqueIdentifier(), andrew2.getUniqueIdentifier());
+        assertEquals(andrew1.getUniqueIdentifier(), andrew3.getUniqueIdentifier());
+        assertEquals(andrew2.getUniqueIdentifier(), andrew3.getUniqueIdentifier());
+        assertEquals(2, andrew1.getChildContacts().size());
+        assertTrue(andrew1.getChildContacts().contains(andrew2));
+        assertTrue(andrew1.getChildContacts().contains(andrew3));
+    }
+
+    @AfterEach
+    public void tearDown(){
+        contacts.clear();
     }
 }
